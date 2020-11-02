1ed42c7bd4881339a155400f27173fd552159925
Varinder singh <varinder83singh@gmail.com>
2020-11-02 07:33:49 -0800

    new

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoStructure from './component/TodoStructure';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> to do</h1>
+      <TodoStructure/>
+    </div>
+  );
+}
+
+export default App;
+
